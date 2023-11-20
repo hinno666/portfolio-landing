b0da56b1d4ecb6d7699a505efd4f746875cfac6e
Karpov Andrey <luffytaroyo@gmail.com>
2023-11-20 11:18:14 +0300

    test commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
         <footer className="bg-emerald-900 text-white shadow-2xl shadow-inner">
             <div className="align-element py-4 sm:py-6 text-center font-bold">
-                Thx for watching!
+                hello everyone!
             </div>
         </footer>
     )
